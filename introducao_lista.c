afc49d144c14022bc1bd93be0035a1f028bf1613
Bernardo Ribeiro Effgen <150496436+Bribeiro13@users.noreply.github.com>
2025-10-08 07:54:17 -0300

    Add files via upload
    Código de introdução a lista encadeada.

---
+++ introducao_lista.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct cel{
+    int conteudo;
+    struct cel *seg;
+}cel;
+typedef struct cel* Lista;
+
+Lista* cria_Lista(){
+    Lista *li = (Lista*) malloc (sizeof(Lista));
+    if(li!=NULL){
+        *li=NULL;
+    }
+    return li;
+}
+int main()
+{
+    Lista *lst;
+    lst = cria_Lista();
+    
+    cel c1, c2, c3;
+    c1.conteudo = 1;
+    c2.conteudo = 2;
+    c3.conteudo = 3;
+    c1.seg = &c2;
+    c2.seg = &c3;
+    c3.seg = NULL;
+    
+    *lst = &c1;
+
+    cel* aux = *lst;
+    
+    printf("Minha Lista:\n");
+    
+    while(aux!=NULL){
+        printf("\t%d", aux->conteudo);
+        aux = aux->seg;
+    }
+
+    return 0;
+}
